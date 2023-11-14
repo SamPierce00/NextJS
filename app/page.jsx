b24903b2f90996d9bbe368683a8ec063bcbde3a2
Sam Pierce <112190589+SamPierce00@users.noreply.github.com>
2023-11-14 15:23:12 -0700

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
 "use client"
 import CountButton1 from "./CountButton1"
 import CountButton5 from "./CountButton5"
+import Link from "next/link"
 export default function Page() {
     return <div>
       <CountButton1 />
       <CountButton5 />
       <hr />
-      <a href="github.com">My repo</a>
+      <a Link href="https://github.com/SamPierce00/NextJS.git" passHref={true}>My repo</a>
       <hr />
       <p>Search Bar</p>
     </div>
